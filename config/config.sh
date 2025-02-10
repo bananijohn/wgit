f291b6b86624f9707e8629b0f4f884848412e98e
Xatra1 <xatra169@gmail.com>
2025-02-10 12:19:44 -0500

    add files

---
+++ config/config.sh
@@ -0,0 +1,17 @@
+# Variables are defined here
+
+# Shortcut for defining variables (optional)
+
+wgitld="/home/${USER}"
+
+# Which directories to run git pull (wgit -p) in:
+gitdirectories=""
+
+# True to display the logo, false to not:
+
+wgitlogo=true
+
+# Logo directory:
+
+# Use confdir for ~/.config
+wgitlogod="${confdir}/wgit/logo.txt"
